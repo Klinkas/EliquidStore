3620cdb1323fa29144dc6a4b74fd502bdb4978d2
Klinkas <versike727@gmail.com>
2024-02-25 20:11:51 +0500

    Added Cors Policy

--- EliquidStore.API/Program.cs
+++ EliquidStore.API/Program.cs
@@ -29,4 +29,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.UseCors(x =>
+{
+    x.WithHeaders().AllowAnyHeader();
+    x.WithOrigins("http://localhost:3000");
+    x.WithMethods().AllowAnyMethod();
+});
 app.Run();
