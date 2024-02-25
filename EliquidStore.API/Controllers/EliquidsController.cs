a144688787baa45e08b94eb2a55e26845d778e9d
Klinkas <versike727@gmail.com>
2024-02-25 15:44:38 +0500

    Created controller `EliquidsController`

---
+++ EliquidStore.API/Controllers/EliquidsController.cs
@@ -0,0 +1,27 @@
+﻿using EliquidStore.API.Contracts;
+using EliquidStore.Application.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EliquidStore.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EliquidsController : ControllerBase
+{
+    private readonly IEliquidsService _eliquidsService;
+
+    public EliquidsController(IEliquidsService eliquidsService)
+    {
+        _eliquidsService = eliquidsService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<EliquidsResponse>>> GetEliquids()
+    {
+        var eliquids = await _eliquidsService.GetAllEliquids();
+
+        var response = eliquids.Select(e => new EliquidsResponse(e.Id, e.Name, e.Flavor, e.Capacity));
+
+        return Ok(response);
+    }
+}
